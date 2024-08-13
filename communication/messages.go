f86dc92038addc6481336f153f4be344278b2725
Paulo Campos <paulocampos.dev@outlook.com>
2024-08-13 12:19:08 -0300

    Arrumei formato da struct mensagem
    Agora ela segue mais fielmente o formato da mensagem que recebemos da
    twilio.

--- communication/messages.go
+++ communication/messages.go
@@ -1,58 +1,38 @@
 package communication
 
+// talvez usar o pacote gorilla/schema seja util ele possui ferramentas para parse de url.values
+// esse é o formato que recebemos das mensagens do Twilio
 type Message struct {
-	AccountSid          string `json:"account_sid"`
-	APIVersion          string `json:"api_version"`
-	Body                string `json:"body"`
-	DateCreated         string `json:"date_created"`
-	DateSent            string `json:"date_sent"`
-	Direction           string `json:"direction"`
-	ErrorCode           string `json:"error_code"`
-	ErrorMessage        string `json:"error_message"`
-	From                string `json:"from"`
-	NumMedia            string `json:"num_media"`
-	NumSegments         string `json:"num_segments"`
-	Price               string `json:"price"`
-	PriceUnit           string `json:"price_unit"`
-	MessagingServiceSid string `json:"messaging_service_sid"`
-	SID                 string `json:"sid"`
-	Status              string `json:"status"`
-	SubresourceUris     string `json:"subresource_uris"`
-	Tags                string `json:"tags"`
-	To                  string `json:"to"`
-	URI                 string `json:"uri"`
+	SmsMessageSid    string `json:"sms_message_sid"`
+	NumMedia         string `json:"num_media"`
+	ProfileName      string `json:"profile_name"`
+	MessageType      string `json:"message_type"`
+	SmsSid           string `json:"sms_sid"`
+	WaId             string `json:"wa_id"`
+	SmsStatus        string `json:"sms_status"`
+	Body             string `json:"body"`
+	To               string `json:"to"`
+	NumSegments      string `json:"num_segments"`
+	ReferralNumMedia string `json:"referral_num_media"`
+	MessageSid       string `json:"message_sid"`
+	AccountSid       string `json:"account_sid"`
+	From             string `json:"from"`
+	APIVersion       string `json:"api_version"`
 }
 
-func NewMessage() *Message {
-	return &Message{}
-}
-
-// Esse é um modelo de como volta a mensagem da API do Whatsapp
-// {
-//   "account_sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//   "api_version": "2010-04-01",
-//   "body": "Hello there!",
-//   "date_created": "Thu, 24 Aug 2023 05:01:45 +0000",
-//   "date_sent": "Thu, 24 Aug 2023 05:01:45 +0000",
-//   "date_updated": "Thu, 24 Aug 2023 05:01:45 +0000",
-//   "direction": "outbound-api",
-//   "error_code": null,
-//   "error_message": null,
-//   "from": "whatsapp:+14155238886",
-//   "num_media": "0",
-//   "num_segments": "1",
-//   "price": null,
-//   "price_unit": null,
-//   "messaging_service_sid": "MGaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//   "sid": "SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//   "status": "queued",
-//   "subresource_uris": {
-//     "media": "/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages/SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Media.json"
-//   },
-//   "tags": {
-//     "campaign_name": "Spring Sale 2022",
-//     "message_type": "cart_abandoned"
-//   },
-//   "to": "whatsapp:+15005550006",
-//   "uri": "/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages/SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.json"
-// }
+// Este é o formato de uma mensagem que vem da Twilio quando recebemos uma mensagem no Bot
+// SmsMessageSid    = XXXXXXXXXXXXXXXXXXXXXXXX
+// NumMedia         = 0
+// ProfileName      = Paulo Campos
+// MessageType      = text
+// SmsSid           = XXXXXXXXXXXXXXXXXXXXXXXX
+// WaId             = 5511944924454
+// SmsStatus        = received
+// Body             = oi
+// To               = whatsapp%!A(MISSING)%!B(MISSING)14155238886
+// NumSegments      = 1
+// ReferralNumMedia = 0
+// MessageSid       = XXXXXXXXXXXXXXXXXXXXXXXX
+// AccountSid       = XXXXXXXXXXXXXXXXXXXXXXXX
+// From             = whatsapp%!A(MISSING)%!B(MISSING)5511944924454
+// ApiVersion       = 2010-04-01
